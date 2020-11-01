d22c07abe90dd7abdab0161308c655720eb136e8
akshay0300995 <44026610+akshay0300995@users.noreply.github.com>
2020-11-01 20:20:52 +0530

    Update runScript2.sh

--- runScript2.sh
+++ runScript2.sh
@@ -1,1 +1,7 @@
-echo akshay123
+PID=`ps -eaf | grep 5000 | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$PID" ]]; then
+  echo "killing $PID"
+  echo $PID
+  kill -9 $PID
+fi
+nohup python /home/ubuntu/python_project.py
