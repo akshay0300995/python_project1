ca0f0eb3f6c3b9afbf8b22c6bb9c25681d37df10
akshay0300995 <44026610+akshay0300995@users.noreply.github.com>
2020-11-01 21:38:11 +0530

    Update runScript2.sh

--- runScript2.sh
+++ runScript2.sh
@@ -1,7 +1,2 @@
-sudo PID=`ps -eaf | grep 5000 | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  sudo echo "killing $PID"
-  sudo echo $PID
-   sudo kill -9 $PID
-fi
+sudo pkill screen
 sudo screen -m -d python /home/ubuntu/python_project/new.py
