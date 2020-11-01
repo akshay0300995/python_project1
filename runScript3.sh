7786a42a3c1227f1c41f197322f8c7c970e3c87f
akshay0300995 <44026610+akshay0300995@users.noreply.github.com>
2020-11-01 20:39:27 +0530

    Update runScript3.sh

--- runScript3.sh
+++ runScript3.sh
@@ -1,1 +1,9 @@
-echo akshay
+log_time=`date +"%T"`
+mv /home/ubuntu/python_project/nohup.out /home/ubuntu/python_project/nohup_$log_time 
+
+PID=`sudo ps -eaf | grep 5000 | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$PID" ]]; then
+  sudo echo "killing $PID"
+  sudo echo $PID
+   sudo kill -9 $PID
+fi
