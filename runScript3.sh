81eb67fcbb64fd1613d69c5b8c6f7e5783a0a7c9
akshay0300995 <44026610+akshay0300995@users.noreply.github.com>
2020-11-02 10:40:02 +0530

    Update runScript3.sh

--- runScript3.sh
+++ runScript3.sh
@@ -1,9 +1,1 @@
-log_time=`date +"%T"`
-mv /home/ubuntu/python_project/nohup.out /home/ubuntu/python_project/nohup_$log_time 
-
-PID=`sudo ps -eaf | grep 5000 | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  sudo echo "killing $PID"
-  sudo echo $PID
-   sudo kill -9 $PID
-fi
+sudo pkill screen
